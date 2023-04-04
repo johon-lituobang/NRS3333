2e72849de1c096210b9b88ffe46dd79d974d16e3
tubanlee <107244343+tubanlee@users.noreply.github.com>
2023-04-04 04:17:00 -0700

    Add files via upload

---
+++ 9_Weibull_compare.R
@@ -0,0 +1,311 @@
+#Copyright 2023 Tuban Lee
+
+#These codes and manuscripts are under review in PNAS, please do not share them.
+
+#If you are interested, please do not hesitate to contact me. Cooperation is also welcomed!
+
+#require foreach and doparallel for parallel processing of bootstrap (not available for some types of computers)
+if (!require("foreach")) install.packages("foreach")
+library(foreach)
+if (!require("doParallel")) install.packages("doParallel")
+library(doParallel)
+#require randtoolbox for random number generations
+if (!require("randtoolbox")) install.packages("randtoolbox")
+library(randtoolbox)
+if (!require("Rcpp")) install.packages("Rcpp")
+library(Rcpp)
+if (!require("Rfast")) install.packages("Rfast")
+library(Rfast)
+if (!require("NRSReview")) install.packages("NRSReview_1.0.tar.gz", repos = NULL)
+library(NRSReview)
+if (!require("matrixStats")) install.packages("matrixStats")
+library(matrixStats)
+
+
+numCores <- 124
+#registering clusters, can set a smaller number using numCores-1
+
+registerDoParallel(numCores)
+
+
+#bootsize for bootstrap approximation of the distributions of the kernel of U-statistics.
+n <- 1.8*10^4
+(n%%10)==0
+# maximum order of moments
+morder <- 4
+
+#generate quasirandom numbers based on the Sobol sequence
+quasiunisobol<-sobol(n=n, dim = morder, init = TRUE, scrambling = 0, seed = NULL, normal = FALSE,
+                     mixed = FALSE, method = "C", start = 1)
+quasiuni<-rbind(quasiunisobol)
+
+quasiunisobol<-c()
+
+quasiuni_sorted2 <- na.omit(rowSort(quasiuni[,1:2], descend = FALSE, stable = FALSE, parallel = TRUE))
+quasiuni_sorted3 <- na.omit(rowSort(quasiuni[,1:3], descend = FALSE, stable = FALSE, parallel = TRUE))
+quasiuni_sorted4 <- na.omit(rowSort(quasiuni, descend = FALSE, stable = FALSE, parallel = TRUE))
+# Forever...
+
+asymptotic_n <- 1.8*10^6
+(asymptotic_n%%10)==0
+# maximum order of moments
+morder <- 4
+#large sample size (asymptotic bias)
+largesize<-1.8*10^6
+
+#generate quasirandom numbers based on the Sobol sequence
+quasiunisobol_asymptotic<-sobol(n=asymptotic_n, dim = morder, init = TRUE, scrambling = 0, seed = NULL, normal = FALSE,
+                                mixed = FALSE, method = "C", start = 1)
+
+quasiuni_asymptotic<-rbind(quasiunisobol_asymptotic)
+
+quasiunisobol_asymptotic<-c()
+
+quasiuni_sorted2_asymptotic <- na.omit(rowSort(quasiuni_asymptotic[,1:2], descend = FALSE, stable = FALSE, parallel = TRUE))
+quasiuni_sorted3_asymptotic <- na.omit(rowSort(quasiuni_asymptotic[,1:3], descend = FALSE, stable = FALSE, parallel = TRUE))
+quasiuni_sorted4_asymptotic <- na.omit(rowSort(quasiuni_asymptotic, descend = FALSE, stable = FALSE, parallel = TRUE))
+# Forever...
+
+quasiuni_asymptotic<-Sort(quasiuni_asymptotic[,1])
+
+orderlist1_AB2_asymptotic<-createorderlist(quni1=quasiuni_sorted2_asymptotic,size=largesize,interval=8,dimension=2)
+orderlist1_AB3_asymptotic<-createorderlist(quni1=quasiuni_sorted3_asymptotic,size=largesize,interval=8,dimension=3)
+orderlist1_AB4_asymptotic<-createorderlist(quni1=quasiuni_sorted4_asymptotic,size=largesize,interval=8,dimension=4)
+
+quasiuni_sorted2_asymptotic<-c()
+quasiuni_sorted3_asymptotic<-c()
+quasiuni_sorted4_asymptotic<-c()
+
+#load asymptotic d for two parameter distributions
+d_values<- read.csv(("d_SWA.csv"))
+I_values<- read.csv(("I_SWA.csv"))
+
+#set the stop criterion
+criterionset=1e-30
+
+kurtWeibull<- read.csv(("kurtWeibull_31150.csv"))
+allkurtWeibull<-unlist(kurtWeibull)
+
+simulatedbatch_asymptoticbias<-foreach(batchnumber = (1:length(allkurtWeibull)), .combine = 'rbind') %dopar% {
+  library(Rfast)
+  library(matrixStats)
+  library(NRSReview)
+  a=allkurtWeibull[batchnumber]
+  x<-c(dsWeibull(uni=quasiuni_asymptotic, shape=a/1, scale = 1))
+  targetm<-gamma(1+1/(a/1))
+  targetvar<-(gamma(1+2/(a/1))-(gamma(((1+1/(a/1)))))^2)
+  targettm<-((sqrt(gamma(1+2/(a/1))-(gamma(((1+1/(a/1)))))^2))^3)*(gamma(1+3/(a/1))-3*(gamma(1+1/(a/1)))*((gamma(1+2/(a/1))))+2*((gamma(1+1/(a/1)))^3))/((sqrt(gamma(1+2/(a/1))-(gamma(((1+1/(a/1)))))^2))^(3))
+  targetfm<-((sqrt(gamma(1+2/(a/1))-(gamma(((1+1/(a/1)))))^2))^4)*(gamma(1+4/(a/1))-4*(gamma(1+3/(a/1)))*((gamma(1+1/(a/1))))+6*(gamma(1+2/(a/1)))*((gamma(1+1/(a/1)))^2)-3*((gamma(1+1/(a/1)))^4))/(((gamma(1+2/(a/1))-(gamma(((1+1/(a/1)))))^2))^(2))
+  kurtx<-targetfm/(targetvar^(4/2))
+  skewx<-targettm/(targetvar^(3/2))
+  sortedx<-Sort(x,descending=FALSE,partial=NULL,stable=FALSE,na.last=NULL)
+
+  targetall<-c(targetm=targetm,targetvar=targetvar,targettm=targettm,targetfm=targetfm)
+  x<-c()
+  Huberx<-Huber_estimator(x=sortedx)
+  #D Olive, Robust estimators for transformed location scale families. Unpubl. manuscript 1025 available from (www. math. siu. edu/olive/preprints. htm) (2006).
+  medianMAD1<-Weibull_median_MAD_estimator(sortedx)
+  
+  #NB Marks, Estimation of weibull parameters from common percentiles. J. applied Stat. 32, 17–24 (2005). 
+  QE1<-Weibull_quantile_estimator(x=sortedx,sorted=TRUE)
+  
+  alpha1<-QE1[1]-0.3
+  alpha2<-QE1[1]+0.3
+  
+  #X He, WK Fung, Method of medians for lifetime data with weibull models. Stat. medicine 18, 1993–2009 (1999)
+  RMLE1<-Weibull_RMLE(sortedx,alpha1=alpha1,alpha2=alpha2)
+  
+  #all parameter setting are from
+  #K Boudt, D Caliskan, C Croux, Robust explicit estimators of weibull parameters. Metrika 73, 187–209 (2011).
+  
+  moments_medianMAD1<-Weibull_moments(alpha=medianMAD1[1],lambda=medianMAD1[2])
+  
+  moments_QE1<-Weibull_moments(alpha=QE1[1],lambda=QE1[2])
+  
+  moments_RMLE1<-Weibull_moments(alpha=RMLE1[1],lambda=RMLE1[2])
+
+  momentsx<-unbiasedmoments(x=sortedx)
+
+  sortedx<-c()
+  all1<-t(c(kurtx,skewx,Huberx,targetall,momentsx,moments_medianMAD1=moments_medianMAD1,moments_QE1=moments_QE1,moments_RMLE1=moments_RMLE1))
+}
+
+write.csv(simulatedbatch_asymptoticbias,paste("asymptotic_compare_Weibull_raw_Process",largesize,".csv", sep = ","), row.names = FALSE)
+
+kurtgamma<- read.csv(("kurtgamma_31150.csv"))
+allkurtgamma<-unlist(kurtgamma)
+
+simulatedbatch_asymptoticbias<-foreach(batchnumber = (1:length(allkurtgamma)), .combine = 'rbind') %dopar% {
+  library(Rfast)
+  library(matrixStats)
+  library(NRSReview)
+  a=allkurtgamma[batchnumber]
+  x<-c(dsgamma(uni=quasiuni_asymptotic, shape=a/1, rate  = 1))
+  targetm<-a
+  targetvar<-(a)
+  targettm<-((sqrt(a))^3)*2/sqrt(a)
+  targetfm<-((sqrt(a))^4)*((6/(a))+3)
+  kurtx<-targetfm/(targetvar^(4/2))
+  skewx<-targettm/(targetvar^(3/2))
+  sortedx<-Sort(x,descending=FALSE,partial=NULL,stable=FALSE,na.last=NULL)
+
+  targetall<-c(targetm=targetm,targetvar=targetvar,targettm=targettm,targetfm=targetfm)
+  x<-c()
+  
+  Huberx<-Huber_estimator(x=sortedx)
+  
+  medianMAD1<-Weibull_median_MAD_estimator(sortedx)
+  
+  QE1<-Weibull_quantile_estimator(x=sortedx,sorted=TRUE)
+  
+  alpha1<-QE1[1]-0.3
+  alpha2<-QE1[1]+0.3
+  
+  RMLE1<-Weibull_RMLE(sortedx,alpha1=alpha1,alpha2=alpha2)
+  
+  moments_medianMAD1<-Weibull_moments(alpha=medianMAD1[1],lambda=medianMAD1[2])
+  
+  moments_QE1<-Weibull_moments(alpha=QE1[1],lambda=QE1[2])
+  
+  moments_RMLE1<-Weibull_moments(alpha=RMLE1[1],lambda=RMLE1[2])
+  
+  momentsx<-unbiasedmoments(x=sortedx)
+  
+  sortedx<-c()
+  all1<-t(c(kurtx,skewx,Huberx,targetall,momentsx,moments_medianMAD1=moments_medianMAD1,moments_QE1=moments_QE1,moments_RMLE1=moments_RMLE1))
+}
+
+write.csv(simulatedbatch_asymptoticbias,paste("asymptotic_compare_gamma_raw_Process",largesize,".csv", sep = ","), row.names = FALSE)
+
+kurtPareto<- read.csv(("kurtPareto_91210.csv"))
+allkurtPareto<-unlist(kurtPareto)
+
+simulatedbatch_asymptoticbias<-foreach(batchnumber = (1:length(allkurtPareto)), .combine = 'rbind') %dopar% {
+  library(Rfast)
+  library(matrixStats)
+  library(NRSReview)
+  a=allkurtPareto[batchnumber]
+  x<-c(dsPareto(uni=quasiuni_asymptotic, shape=a/1, scale = 1))
+
+  targetm<-a/(a-1)
+  targetvar<-(((a))*(1)/((-2+(a))*((-1+(a))^2)))
+  targettm<-((((a)+1)*(2)*(sqrt(a-2)))/((-3+(a))*(((a))^(1/2))))*(((sqrt(((a))*(1)/((-2+(a))*((-1+(a))^2))))^3))
+  targetfm<-(3+(6*((a)^3+(a)^2-6*(a)-2)/(((a))*((-3+(a)))*((-4+(a))))))*((sqrt(((a))*(1)/((-2+(a))*((-1+(a))^2))))^4)
+  kurtx<-targetfm/(targetvar^(4/2))
+  skewx<-targettm/(targetvar^(3/2))
+  sortedx<-Sort(x,descending=FALSE,partial=NULL,stable=FALSE,na.last=NULL)
+
+  targetall<-c(targetm=targetm,targetvar=targetvar,targettm=targettm,targetfm=targetfm)
+  x<-c()
+  
+  Huberx<-Huber_estimator(x=sortedx)
+  
+  medianMAD1<-Weibull_median_MAD_estimator(sortedx)
+  
+  QE1<-Weibull_quantile_estimator(x=sortedx,sorted=TRUE)
+  
+  alpha1<-QE1[1]-0.3
+  alpha2<-QE1[1]+0.3
+  
+  RMLE1<-Weibull_RMLE(sortedx,alpha1=alpha1,alpha2=alpha2)
+  
+  moments_medianMAD1<-Weibull_moments(alpha=medianMAD1[1],lambda=medianMAD1[2])
+  
+  moments_QE1<-Weibull_moments(alpha=QE1[1],lambda=QE1[2])
+  
+  moments_RMLE1<-Weibull_moments(alpha=RMLE1[1],lambda=RMLE1[2])
+  
+  momentsx<-unbiasedmoments(x=sortedx)
+  
+  sortedx<-c()
+  all1<-t(c(kurtx,skewx,Huberx,targetall,momentsx,moments_medianMAD1=moments_medianMAD1,moments_QE1=moments_QE1,moments_RMLE1=moments_RMLE1))
+}
+
+write.csv(simulatedbatch_asymptoticbias,paste("asymptotic_compare_Pareto_raw_Process",largesize,".csv", sep = ","), row.names = FALSE)
+
+
+kurtlognorm<- read.csv(("kurtlognorm_31150.csv"))
+allkurtlognorm<-unlist(kurtlognorm)
+
+
+simulatedbatch_asymptoticbias<-foreach(batchnumber = (1:length(allkurtlognorm)), .combine = 'rbind') %dopar% {
+  library(Rfast)
+  library(matrixStats)
+  library(NRSReview)
+  a=allkurtlognorm[batchnumber]
+  x<-c(dslnorm(uni=quasiuni_asymptotic, meanlog =0, sdlog  = a/1))
+  targetm<-exp((a^2)/2)
+  targetvar<-(exp((a/1)^2)*(-1+exp((a/1)^2)))
+  targettm<-sqrt(exp((a/1)^2)-1)*((2+exp((a/1)^2)))*((sqrt(exp((a/1)^2)*(-1+exp((a/1)^2))))^3)
+  targetfm<-((-3+exp(4*((a/1)^2))+2*exp(3*((a/1)^2))+3*exp(2*((a/1)^2))))*((sqrt(exp((a/1)^2)*(-1+exp((a/1)^2))))^4)
+  kurtx<-targetfm/(targetvar^(4/2))
+  skewx<-targettm/(targetvar^(3/2))
+  sortedx<-Sort(x,descending=FALSE,partial=NULL,stable=FALSE,na.last=NULL)
+
+  targetall<-c(targetm=targetm,targetvar=targetvar,targettm=targettm,targetfm=targetfm)
+  x<-c()
+  
+  Huberx<-Huber_estimator(x=sortedx)
+  
+  medianMAD1<-Weibull_median_MAD_estimator(sortedx)
+  
+  QE1<-Weibull_quantile_estimator(x=sortedx,sorted=TRUE)
+  
+  alpha1<-QE1[1]-0.3
+  alpha2<-QE1[1]+0.3
+  
+  RMLE1<-Weibull_RMLE(sortedx,alpha1=alpha1,alpha2=alpha2)
+  
+  moments_medianMAD1<-Weibull_moments(alpha=medianMAD1[1],lambda=medianMAD1[2])
+  
+  moments_QE1<-Weibull_moments(alpha=QE1[1],lambda=QE1[2])
+  
+  moments_RMLE1<-Weibull_moments(alpha=RMLE1[1],lambda=RMLE1[2])
+  
+  momentsx<-unbiasedmoments(x=sortedx)
+  
+  sortedx<-c()
+  all1<-t(c(kurtx,skewx,Huberx,targetall,momentsx,moments_medianMAD1=moments_medianMAD1,moments_QE1=moments_QE1,moments_RMLE1=moments_RMLE1))
+}
+
+write.csv(simulatedbatch_asymptoticbias,paste("asymptotic_compare_lognorm_raw_Process",largesize,".csv", sep = ","), row.names = FALSE)
+
+kurtgnorm<- read.csv(("kurtgnorm_31150.csv"))
+allkurtgnorm<-unlist(kurtgnorm)
+
+#no resullt for the generalized Gaussian distribution
+
+#since the log function cannot handle negative inputs.
+batchnumber=10
+a=allkurtgnorm[batchnumber]
+x<-c(dsgnorm(uni=quasiuni_asymptotic, shape=a/1, scale = 1))
+
+targetm<-0
+targetvar<-gamma(3/a)/((gamma(1/a)))
+targettm<-0
+targetfm<-((gamma(3/a)/((gamma(1/a))))^2)*gamma(5/a)*gamma(1/a)/((gamma(3/a))^2)
+kurtx<-targetfm/(targetvar^(4/2))
+skewx<-targettm/(targetvar^(3/2))
+sortedx<-Sort(x,descending=FALSE,partial=NULL,stable=FALSE,na.last=NULL)
+
+targetall<-c(targetm=targetm,targetvar=targetvar,targettm=targettm,targetfm=targetfm)
+
+x<-c()
+
+Huberx<-Huber_estimator(x=sortedx)
+
+medianMAD1<-Weibull_median_MAD_estimator(sortedx)
+
+QE1<-Weibull_quantile_estimator(x=sortedx,sorted=TRUE)
+
+alpha1<-QE1[1]-0.3
+alpha2<-QE1[1]+0.3
+
+RMLE1<-Weibull_RMLE(sortedx,alpha1=alpha1,alpha2=alpha2)
+
+moments_medianMAD1<-Weibull_moments(alpha=medianMAD1[1],lambda=medianMAD1[2])
+
+moments_QE1<-Weibull_moments(alpha=QE1[1],lambda=QE1[2])
+
+moments_RMLE1<-Weibull_moments(alpha=RMLE1[1],lambda=RMLE1[2])
+
