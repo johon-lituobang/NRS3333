cd567698c9a85b34d1fe6573441799d68f3106c1
tubanlee <107244343+tubanlee@users.noreply.github.com>
2023-04-09 04:13:02 -0700

    Update 1_Asymptotic_NRSs_d_calibration_SWA.R

--- 1_Asymptotic_NRSs_d_calibration_SWA.R
+++ 1_Asymptotic_NRSs_d_calibration_SWA.R
@@ -26,12 +26,12 @@ cl <- makeCluster(numCores) # Create a cluster with the number of cores
 registerDoParallel(cl) # Register the parallel backend
 
 #bootsize for bootstrap approximation of the distributions of the kernal of U-statistics.
-n <- 65536*30*3
+n <- 65536*50*3
 (n%%10)==0
 # maximum order of moments
 morder <- 4
 #large sample size (approximating asymptotic)
-largesize<-65536*30
+largesize<-65536*50
 
 #generate quasirandom numbers based on the Sobol sequence
 quasiunisobol<-sobol(n=n, dim = morder, init = TRUE, scrambling = 0, seed = NULL, normal = FALSE,
