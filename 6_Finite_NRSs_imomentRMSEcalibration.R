194c47ec1ae0e5a545e0b7e7f81274325b176ec6
tubanlee <107244343+tubanlee@users.noreply.github.com>
2023-04-19 07:53:04 -0700

    Update 6_Finite_NRSs_imomentRMSEcalibration.R

--- 6_Finite_NRSs_imomentRMSEcalibration.R
+++ 6_Finite_NRSs_imomentRMSEcalibration.R
@@ -65,7 +65,7 @@ orderlist1_AB30<-orderlist1_AB30[1:largesize,]
 orderlist1_AB40<-createorderlist(quni1=quasiuni_sorted4,size=samplesize,interval=8,dimension=4)
 orderlist1_AB40<-orderlist1_AB40[1:largesize,]
 
-batchsize=5
+batchsize=batchsizebase
 
 n <- samplesize
 setSeed(1)
@@ -103,7 +103,7 @@ simulatedbatch_bias_Monte<-foreach(batchnumber =c((1:length(allkurtWeibull))), .
     targetall<-c(targetm=targetm,targetvar=targetvar,targettm=targettm,targetfm=targetfm)
     x<-c()
     
-    rqmomentselect1<-rqmoments2(x=sortedx,iall1=iall11,dtype1=1,releaseall=TRUE,standist_d=d_values,standist_I=I_values,orderlist1_sorted20=orderlist1_AB2,orderlist1_sorted30=orderlist1_AB3,orderlist1_sorted40=orderlist1_AB4,percentage=1/24,batch="auto",stepsize=1000,criterion=1e-10,boot=TRUE)
+    rqmomentselect1<-rqmoments2(x=sortedx,iall1=iall11,dtype1=1,releaseall=TRUE,standist_d=d_values,standist_I=I_values,orderlist1_sorted20=orderlist1_AB20,orderlist1_sorted30=orderlist1_AB30,orderlist1_sorted40=orderlist1_AB40,percentage=1/24,batch="auto",stepsize=1000,criterion=1e-10,boot=TRUE)
     
     standardizedmomentsx<-standardizedmoments(x=sortedx)
     
@@ -114,31 +114,25 @@ simulatedbatch_bias_Monte<-foreach(batchnumber =c((1:length(allkurtWeibull))), .
     SEbataches2<-rbind(SEbataches2,all1)
   }
   
-  write.csv(SEbataches2,paste("finite_Weibull_Imomentscalibration_raw",samplesize,round(kurtx,digits = 1),".csv", sep = ","), row.names = FALSE)
+  write.csv(SEbataches2,paste("finite_Weibull_Ismomentscalibration_raw",samplesize,round(kurtx,digits = 1),".csv", sep = ","), row.names = FALSE)
   
   SEbatachesmean <-apply(SEbataches2, 2, calculate_column_mean)
   
-  rqmean<-apply(((SEbataches2[1:batchsize,1:72])), 2, calculate_column_sd)
+  ikurt2<-apply(((SEbataches2[1:batchsize,c(1,3)])), 2, calculate_column_sd)
   
-  rqvar<-apply((SEbataches2[1:batchsize,c(73:124)]), 2, calculate_column_sd)
+  iskew2<-apply((SEbataches2[1:batchsize,c(2,4)]), 2, calculate_column_sd)
   
-  rqtm<-apply((SEbataches2[1:batchsize,c(125:164)]), 2, calculate_column_sd)
+  rankikurt2<-rank(ikurt2)
+  rankiskew2<-rank(iskew2)
   
-  rqfm<-apply((SEbataches2[1:batchsize,c(165:192)]), 2, calculate_column_sd)
-  
-  rankmean1<-rank(rqmean)
-  rankvar1<-rank(rqvar)
-  ranktm1<-rank(rqtm)
-  rankfm1<-rank(rqfm)
-  
-  allresultsSE<-c(samplesize=samplesize,type=1,kurtx,skewx,rankmean1,rankvar1,ranktm1,rankfm1,SEbatachesmean,SErqmean=rqmean,SErqvar=rqvar,SErqtm=rqtm,SErqfm=rqfm)
+  allresultsSE<-c(samplesize=samplesize,type=1,kurtx,skewx,rankikurt2,rankiskew2,SEbatachesmean,SEikurt2=ikurt2,SEiskew2=iskew2)
 }
 
-write.csv(simulatedbatch_bias_Monte,paste("finite_Weibull_Imomentscalibration_raw",samplesize,".csv", sep = ","), row.names = FALSE)
+write.csv(simulatedbatch_bias_Monte,paste("finite_Weibull_Ismomentscalibration_raw",samplesize,".csv", sep = ","), row.names = FALSE)
 
-Optimum_SE<-simulatedbatch_bias_Monte[,1:196]
+Optimum_SE<-simulatedbatch_bias_Monte[,1:8]
 
-write.csv(Optimum_SE,paste("finite_Imoments_Weibull.csv", sep = ","), row.names = FALSE)
+write.csv(Optimum_SE,paste("finite_Ismoments_Weibull.csv", sep = ","), row.names = FALSE)
 
 simulatedbatch_bias_Monte_SE<-foreach(batchnumber =c((1:length(allkurtWeibull))), .combine = 'rbind') %dopar% {
   library(Rfast)
@@ -154,36 +148,32 @@ simulatedbatch_bias_Monte_SE<-foreach(batchnumber =c((1:length(allkurtWeibull)))
   kurtx<-targetfm/(targetvar^(4/2))
   skewx<-targettm/(targetvar^(3/2))
   
-  SEbataches<- read.csv(paste("finite_Weibull_Imomentscalibration_raw",samplesize,round(kurtx,digits = 1),".csv", sep = ","))
+  SEbataches<- read.csv(paste("finite_Weibull_Ismomentscalibration_raw",samplesize,round(kurtx,digits = 1),".csv", sep = ","))
   
   se_mean_all1<-apply((SEbataches[1:batchsize,]), 2, se_mean)
   
-  rqmean_se<-apply(((SEbataches[1:batchsize,1:72])), 2, se_sd)
+  ikurt2_se<-apply(((SEbataches[1:batchsize,c(1,3)])), 2, se_sd)
   
-  rqvar_se<-apply((SEbataches[1:batchsize,c(73:124)]), 2, se_sd)
+  iskew2_se<-apply((SEbataches[1:batchsize,c(2,4)]), 2, se_sd)
   
-  rqtm_se<-apply((SEbataches[1:batchsize,c(125:164)]), 2, se_sd)
-  
-  rqfm_se<-apply((SEbataches[1:batchsize,c(165:192)]), 2, se_sd)
-  
-  allresultsSE<-c(samplesize=samplesize,type=1,kurtx,skewx,se_mean_all1,rqmean_se,rqvar_se,rqtm_se,rqfm_se)
+  allresultsSE<-c(samplesize=samplesize,type=1,kurtx,skewx,se_mean_all1,ikurt2_se,iskew2_se)
   
   allresultsSE
 }
 
-write.csv(simulatedbatch_bias_Monte_SE,paste("finite_Weibull_Imomentscalibration_raw_error",samplesize,".csv", sep = ","), row.names = FALSE)
+write.csv(simulatedbatch_bias_Monte_SE,paste("finite_Weibull_Ismomentscalibration_raw_error",samplesize,".csv", sep = ","), row.names = FALSE)
 
-finite_I_Weibull<- read.csv(("finite_Imoments_Weibull.csv"))
+finite_I_Weibull<- read.csv(("finite_Ismoments_Weibull.csv"))
 
 
 finite_I_Weibull<-data.frame(finite_I_Weibull)
 
-asymptotic_I_Weibull<- read.csv(paste("asymptotic_Imoments.csv", sep = ","))
+asymptotic_I_Weibull<- read.csv(paste("asymptotic_Ismoments.csv", sep = ","))
 colnames(asymptotic_I_Weibull)<-colnames(finite_I_Weibull)
 
 all1<-rbind(asymptotic_I_Weibull,finite_I_Weibull)
 
-write.csv(all1,paste("Imoments_values.csv", sep = ","), row.names = FALSE)
+write.csv(all1,paste("Ismoments_values.csv", sep = ","), row.names = FALSE)
 
 stopCluster(cl)
 registerDoSEQ()
