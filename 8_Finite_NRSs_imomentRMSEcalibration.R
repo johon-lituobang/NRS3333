6f711b9e73bec18240532e201973fbf8c79e5f41
tubanlee <107244343+tubanlee@users.noreply.github.com>
2023-04-19 13:51:47 -0700

    Update 8_Finite_NRSs_imomentRMSEcalibration.R

--- 8_Finite_NRSs_imomentRMSEcalibration.R
+++ 8_Finite_NRSs_imomentRMSEcalibration.R
@@ -121,13 +121,13 @@ simulatedbatch_bias_Monte<-foreach(batchnumber =c((1:length(allkurtWeibull))), .
   
   SEbatachesmean <-apply(SEbataches3, 2, calculate_column_mean)
   
-  rqmean<-apply(((SEbataches3[1:batchsize,c(3:74,195:266,387:458,579:650)])), 2, calculate_column_sd)
+  rqmean<-apply(((SEbataches3[1:batchsize,c(3:74,195:266,387:458,579:650,771:842,963:1034,1155:1226,1347:1418,1539:1610)])), 2, calculate_column_sd)
   
-  rqvar<-apply((SEbataches3[1:batchsize,c(75:126,267:318,459:510,651:702)]), 2, calculate_column_sd)
+  rqvar<-apply((SEbataches3[1:batchsize,c(75:126,267:318,459:510,651:702,843:894,1035:1086,1227:1278,1419:1470,1611:1662)]), 2, calculate_column_sd)
   
-  rqtm<-apply((SEbataches3[1:batchsize,c(127:166,319:358,511:550,703:742)]), 2, calculate_column_sd)
+  rqtm<-apply((SEbataches3[1:batchsize,c(127:166,319:358,511:550,703:742,895:934,1087:1126,1279:1318,1471:1510,1663:1702)]), 2, calculate_column_sd)
   
-  rqfm<-apply((SEbataches3[1:batchsize,c(167:194,359:386,551:578,743:770)]), 2, calculate_column_sd)
+  rqfm<-apply((SEbataches3[1:batchsize,c(167:194,359:386,551:578,743:770,935:962,1127:1154,1319:1346,1511:1538,1703:1730)]), 2, calculate_column_sd)
   
   rankmean1<-rank(rqmean)
   rankvar1<-rank(rqvar)
@@ -163,13 +163,13 @@ simulatedbatch_bias_Monte_SE<-foreach(batchnumber =c((1:length(allkurtWeibull)))
   se_mean_all1<-apply((SEbataches[1:batchsize,]), 2, se_mean)
   
   
-  rqmean_se<-apply(((SEbataches[1:batchsize,c(3:74,195:266,387:458,579:650)])), 2, se_sd)
+  rqmean_se<-apply(((SEbataches[1:batchsize,c(3:74,195:266,387:458,579:650,771:842,963:1034,1155:1226,1347:1418,1539:1610)])), 2, se_sd)
   
-  rqvar_se<-apply((SEbataches[1:batchsize,c(75:126,267:318,459:510,651:702)]), 2, se_sd)
+  rqvar_se<-apply((SEbataches[1:batchsize,c(75:126,267:318,459:510,651:702,843:894,1035:1086,1227:1278,1419:1470,1611:1662)]), 2, se_sd)
   
-  rqtm_se<-apply((SEbataches[1:batchsize,c(127:166,319:358,511:550,703:742)]), 2, se_sd)
+  rqtm_se<-apply((SEbataches[1:batchsize,c(127:166,319:358,511:550,703:742,895:934,1087:1126,1279:1318,1471:1510,1663:1702)]), 2, se_sd)
   
-  rqfm_se<-apply((SEbataches[1:batchsize,c(167:194,359:386,551:578,743:770)]), 2, se_sd)
+  rqfm_se<-apply((SEbataches[1:batchsize,c(167:194,359:386,551:578,743:770,935:962,1127:1154,1319:1346,1511:1538,1703:1730)]), 2, se_sd)
   
   allresultsSE<-c(samplesize=samplesize,type=1,kurtx,skewx,se_mean_all1,rqmean_se,rqvar_se,rqtm_se,rqfm_se)
   
