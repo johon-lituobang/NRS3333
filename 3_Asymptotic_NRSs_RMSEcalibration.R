e6d931f517f47194e72ec060007b22326a15f199
tubanlee <107244343+tubanlee@users.noreply.github.com>
2023-04-22 01:42:33 -0700

    Update 3_Asymptotic_NRSs_RMSEcalibration.R

--- 3_Asymptotic_NRSs_RMSEcalibration.R
+++ 3_Asymptotic_NRSs_RMSEcalibration.R
@@ -278,7 +278,7 @@ asymptotic_I_lognorm<- read.csv(("asymptotic_I_lognorm.csv"))
 names(asymptotic_I_lognorm)<-1:ncol(asymptotic_I_lognorm)
 names(asymptotic_I_gnorm)<-1:ncol(asymptotic_I_lognorm)
 asymptotic_I_lognorm<-rbind(asymptotic_I_lognorm,asymptotic_I_gnorm)
-asymptotic_I_gnorm<- read.csv(("asymptotic_I_gnorm.csv"))
+asymptotic_I_gnorm<- read.csv(("asymptotic_I_lognorm.csv"))
 names(asymptotic_I_lognorm)<-names(asymptotic_I_gnorm)
 write.csv(asymptotic_I_lognorm,paste("asymptotic_I.csv", sep = ","), row.names = FALSE)
 
